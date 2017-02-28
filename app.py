e4a4ac675b2194a58736cc5adf1dcb8912f4110b
Eligio Becerra <eligio_bz@mail.com>
2017-02-28 14:38:41 -0600

    android project created

--- app.py
+++ app.py
@@ -33,10 +33,12 @@ class User(db.Model):
         self.level = level
 
     def __repr__(self):
-        return {'username': self.username, 'pass': self.password}
+        return {'username': self.username, 'pass': self.password, 'level': self.level}
 
     def getUser(self):
-        return {'username': self.username, 'pass': self.password}
+        return {'username': self.username, 'pass': self.password, 'level': self.level}
+
+
 
 class Product(db.Model):
     barcode = db.Column(db.Integer, primary_key=True)
@@ -70,9 +72,14 @@ class Sale (db.Model):
         return {'sale': self.id, 'date': self.date}
 
 class SaleDetails(db.Model):
+    __tablename__ = "SaleDetails"
+    __table_args__ = (
+        db.PrimaryKeyConstraint('idSale', 'idProduct'),
+    )
     idSale = db.Column(db.Integer)
     idProduct = db.Column(db.Integer)
-    productPrice = db.Column(db.Integer)
+    productPrice = db.Column(db.Float)
+    units = db.Column(db.Integer)
 
     """List of all the available ingredients"""
     def __init__(self, idSale, idProduct, productPrice):
@@ -88,7 +95,7 @@ class SaleDetails(db.Model):
 
 @auth.get_password
 def get_password(em):
-    user = User.query.filter_by(email=em).first()
+    user = User.query.filter_by(username=em).first()
     if user == None:
         return None
     return user.password
@@ -99,122 +106,149 @@ def unauthorized():
 
 ################################## ORDERS API ##################################
 
-@app.route('/salads/api/v1.0/salads', methods=['GET'])
+@app.route('/mobilerp/api/v1.0/findProduct/<int:bCode>', methods=['GET'])
 @auth.login_required
-def get_all_salads():
-    salads = CustomSalad.query.all()
-    return make_response(jsonify({'salads' : [s.serialize for s in salads]}), 200)
-
-@app.route('/salads/api/v1.0/salad/<int:saladId>', methods=['GET'])
-@auth.login_required
-def get_salad(saladId):
-    salad = CustomSalad.query.filter_by(id=saladId).first()
-    if salad is None:
+def findProduct(bCode):
+    product = Product.query.filter_by(barcode=bCode).first()
+    if (product == None):
         abort(404)
-    return make_response(jsonify({'salads' : [salad.serialize]}), 200)
-
-@app.route('/salads/api/v1.0/salad/', methods=['POST'])
-@auth.login_required
-def new_salad():
-    if not request.json:
-        abort(418)
-    if not 'ingredients' in request.json:
-        abort(406)
-    new_salad = None
-    if 'name' in request.json:
-        print ("Name in request")
-        new_salad = CustomSalad(request.json['name'])
     else:
-        print ("No name in request")
-        new_salad = CustomSalad(str(time.time()))
-    db.session.add(new_salad)
-    for ingredient in request.json['ingredients']:
-        ing_exist = Ingredients.query.filter_by(id=ingredient).first()
-        if (ing_exist == None):
-            return make_response(jsonify({'error': "This ingredient doesn't exist"}), 406)
+        return make_response(jsonify({'mobilerp' : [product.serialize]}), 200)
+
+@app.route('/mobilerp/api/v1.0/listProducts/', methods=['GET'])
+@auth.login_required
+def listProducts():
+    products = Product.query.all()
+    if products is None:
+        abort(400)
+    return make_response(jsonify({'mobilerp' : [p.serialize for p in products]}), 200)
+
+@app.route('/mobilerp/api/v1.0/newProduct/', methods=['POST'])
+@auth.login_required
+def newProduct():
+    if not request.json or not 'barcode' in request.json or not 'units' in request.json or not 'price' in request.json or not 'name' in request.json:
+        abort(400)
+    p = Product(request.json['barcode'], request.json['name'], 
+        request.json['units'], request.json['price'])
+    db.session.add(p)
     db.session.commit()
-    for ingredient in request.json['ingredients']:
-        c_salad = saladIngredients(new_salad.id, ingredient)
-        db.session.add(c_salad)
-        db.session.commit()
-    return make_response(jsonify({'salad': new_salad.id}), 201)
+    return make_response(jsonify({'mobilerp' : [p.serialize]}), 200)
 
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
-
-@app.route('/salads/api/v1.0/order', methods=['GET'])
+@app.route('/mobilerp/api/v1.0/updateProduct/<int:bCode>', methods=['PUT'])
 @auth.login_required
-def get_all_orders():
-    orders = Order.query.all()
-    return make_response(jsonify({'orders' : [o.serialize for o in orders]}), 200)
-
-@app.route('/salads/api/v1.0/order', methods=['POST'])
-@auth.login_required
-def new_order():
+def updateProduct(bCode):
     if not request.json:
-        abort(418)
-    if not 'entryTime' in request.json:
-        print ("No entry time")
-        abort(406)
-    if not 'sendAddress' in request.json:
-        print ("No address")
-        abort(406)
-    new_order = None
-    if 'deliveryTime' in request.json:
-        print ("deliveryTime in request")
-        #Fri, 25 Nov 2016 23:17:09 GMT
-        new_order = Order(
-            datetime.datetime.strptime(request.json['entryTime'],
-                "%a, %d %b %Y %H:%M:%S %Z"),
-            request.json['sendAddress'], 
-            datetime.datetime.strptime(request.json['deliveryTime'],
-                "%a, %d %b %Y %H:%M:%S %Z"))
-    else:
-        print ("No deliveryTime in request")
-        new_order = Order(
-            datetime.datetime.strptime(request.json['entryTime'],
-                "%a, %d %b %Y %H:%M:%S %Z"),
-            request.json['sendAddress'])
-    db.session.add(new_order)
-    user = User.query.filter_by(email=auth.username()).first()
-    userOrder = UserOrder(user.id, new_order.id)
-    db.session.add(UserOrder(user.id, new_order.id))
-    db.session.commit()
-    return make_response(jsonify({'order': new_order.id}), 201)
-
-@app.route('/salads/api/v1.0/order/<int:orderId>', methods=['GET'])
-@auth.login_required
-def get_order(orderId):
-    order = Order.query.filter_by(id=orderId).first()
-    if order is None:
+        abort(400)
+    p = Product.query.filter_by(barcode=bCode).first()
+    if p is None:
         abort(404)
-    return make_response(jsonify({'order' : [order.serialize]}), 200)
+    if 'price' in request.json:
+        p.price = float(request.json['price'])
+    if 'units' in request.json :
+        p.units = p.units + int(request.json['units'])
+    db.session.commit()
+    return make_response(jsonify({'mobilerp' : [p.serialize]}), 200)
 
-@app.route('/salads/api/v1.0/order/<int:order_id>', methods=['PUT'])
+@app.route('/mobilerp/api/v1.0/makeSale', methods=['POST'])
 @auth.login_required
-def update_order(order_id):
-    return (jsonify({'salads':'Not Implemented'}), 412)
+def makeSale():
+    if not request.json:
+        abort(400)
+    if 'barcode' not in request.json or len(request.json['barcode']) <= 0:
+        abort(400)
+    s = Sale()
+    db.session.add(s)
+    db.session.commit()
+    for bCode in request.json['barcode']:
+        print(bCode)
+        ps = Product.query.filter_by(barcode=int(bCode)).first()
+        if (ps.units - 1 < 0):
+            abort(406)
+        else:
+            sd = SaleDetails(s.id, ps.barcode, ps.price)
+            db.session.add(sd)
+            db.session.commit()
+    return make_response(jsonify({'mobilerp' : '[p.serialize]'}), 200)
 
-@app.route('/salads/api/v1.0/order/<int:order_id>', methods=['DELETE'])
-@auth.login_required
-def delete_order(order_id):
-    return jsonify({'salads':'Not Implemented'})
+# @app.route('/mobilerp/api/v1.0/salad/', methods=['POST'])
+# @auth.login_required
+# def new_salad():
+#     if not request.json:
+#         abort(418)
+#     if not 'ingredients' in request.json:
+#         abort(406)
+#     new_salad = None
+#     if 'name' in request.json:
+#         print ("Name in request")
+#         new_salad = CustomSalad(request.json['name'])
+#     else:
+#         print ("No name in request")
+#         new_salad = CustomSalad(str(time.time()))
+#     db.session.add(new_salad)
+#     for ingredient in request.json['ingredients']:
+#         ing_exist = Ingredients.query.filter_by(id=ingredient).first()
+#         if (ing_exist == None):
+#             return make_response(jsonify({'error': "This ingredient doesn't exist"}), 406)
+#     db.session.commit()
+#     for ingredient in request.json['ingredients']:
+#         c_salad = saladIngredients(new_salad.id, ingredient)
+#         db.session.add(c_salad)
+#         db.session.commit()
+#     return make_response(jsonify({'salad': new_salad.id}), 201)
+
+# @app.errorhandler(404)
+# def not_found(error):
+#     return make_response(jsonify({'error': 'Not found'}), 404)
+
+# @app.route('/mobilerp/api/v1.0/order', methods=['POST'])
+# @auth.login_required
+# def new_order():
+#     if not request.json:
+#         abort(418)
+#     if not 'entryTime' in request.json:
+#         print ("No entry time")
+#         abort(406)
+#     if not 'sendAddress' in request.json:
+#         print ("No address")
+#         abort(406)
+#     new_order = None
+#     if 'deliveryTime' in request.json:
+#         print ("deliveryTime in request")
+#         #Fri, 25 Nov 2016 23:17:09 GMT
+#         new_order = Order(
+#             datetime.datetime.strptime(request.json['entryTime'],
+#                 "%a, %d %b %Y %H:%M:%S %Z"),
+#             request.json['sendAddress'], 
+#             datetime.datetime.strptime(request.json['deliveryTime'],
+#                 "%a, %d %b %Y %H:%M:%S %Z"))
+#     else:
+#         print ("No deliveryTime in request")
+#         new_order = Order(
+#             datetime.datetime.strptime(request.json['entryTime'],
+#                 "%a, %d %b %Y %H:%M:%S %Z"),
+#             request.json['sendAddress'])
+#     db.session.add(new_order)
+#     user = User.query.filter_by(email=auth.username()).first()
+#     userOrder = UserOrder(user.id, new_order.id)
+#     db.session.add(UserOrder(user.id, new_order.id))
+#     db.session.commit()
+#     return make_response(jsonify({'order': new_order.id}), 201)
 
 ################################## USERS API ##################################
 
 ## New User
-@app.route('/salads/api/v1.0/users', methods=['POST'])
+@app.route('/mobilerp/api/v1.0/users', methods=['POST'])
 def add_user():
-    if not request.json or (not 'user' in request.json and not 'pass' in request.json):
+    if not request.json or (not 'user' in request.json and not 'pass' in request.json 
+        or not 'level' in request.json):
         abort(403)
-    user = User(request.json['user'], request.json['pass'],)
+    user = User(request.json['user'], request.json['pass'], request.json['level'])
     db.session.add(user)
     db.session.commit()
-    return jsonify({'user':user.getUser()})
+    return jsonify({'mobilerp':user.getUser()})
 
 ## Update User
-@app.route('/salads/api/v1.0/users/<string:n_pass>', methods=['PUT'])
+@app.route('/mobilerp/api/v1.0/users/<string:n_pass>', methods=['PUT'])
 @auth.login_required
 def update_pass(n_pass):
     user = User.query.filter_by(email=auth.username()).first()
@@ -230,38 +264,11 @@ def index():
 #### FOR DEBUGGING PURPOSES ###########
 
 def checkDB():
-    if not os.path.isfile("salads.db"):
+    if not os.path.isfile("mobilerp.db"):
         db.create_all()
-        #user1 = User('foo@example.com', 'foopass')
-        #user2 = User('man@example.com', 'manpass')
-        #ingre1 = Ingredients('tomatoCherryA', 'A delicious tomato A')
-        #ingre2 = Ingredients('tomatoCherryB', 'A delicious tomato B')
-        #cs1 = CustomSalad('salad1')
-        #db.session.add(user1)
-        #db.session.add(user2)
-        #db.session.add(ingre1)
-        #db.session.add(ingre2)
-        #db.session.add(cs1)
-        #db.session.commit()
-        #si1 = saladIngredients(cs1.id, ingre1.id)
-        #si2 = saladIngredients(cs1.id, ingre2.id)
-        #db.session.add(si1)
-        #db.session.add(si2)
-        #db.session.commit()
-        #or1 = Order(datetime.datetime.now(), 'Mi casa', datetime.datetime.now())
-        #db.session.add(or1)
-        #db.session.commit()
-        #orsal = OrderSalad(or1.id, cs1.id)
-        #userord = UserOrder(user1.id, or1.id)
-        #db.session.add(orsal)
-        #db.session.add(userord)
-        #db.session.commit()
-        #userord2 = UserOrder(user2.id, or1.id)
-        #db.session.add(userord2)
-        #db.session.commit()
 
 ################################
 
 if __name__ == '__main__':
-    checkDB()
+    #checkDB()
     app.run(host='0.0.0.0', debug=True)
