7bafb42f7ee6fe06d8195e01c75c7970dd54f764
Eligio Becerra <eligio_bz@mail.com>
2017-08-02 18:52:56 -0500

    Started work on depleted report template. Sales report still needs more styling.

--- pdfgenerator.py
+++ pdfgenerator.py
@@ -18,12 +18,21 @@ from jinja2 import Environment, FileSystemLoader
 from weasyprint import HTML
 
 SALES_REPORT_TEMPLATE = 'templates/pdf/sales_report.html'
-SALES_REPORT_STYLE = 'static/css/report_style.css'
+SALES_REPORT_STYLE = 'static/css/sales_report_style.css'
+SALES_REPORT_TEMPLATE = 'templates/pdf/depleted_product.html'
+SALES_REPORT_STYLE = 'static/css/depleted_report_style.css'
 OUTPUT_FOLDER = 'static/pdf/'
 
+env = Environment(loader = FileSystemLoader('.'), extensions=['jinja2.ext.with_'])
+
 def generateSalesPdf(data):
-	env = Environment(loader = FileSystemLoader('.'))
 	template = env.get_template(SALES_REPORT_TEMPLATE)
 	template_vars = data
 	html_output = template.render(template_vars)
-	HTML(string=html_output).write_pdf(OUTPUT_FOLDER + data['title'] + ".pdf", stylesheets=[SALES_REPORT_STYLE])
+	HTML(string=html_output).write_pdf(OUTPUT_FOLDER + data['title'] + ".pdf", stylesheets=[DEPLETED_REPORT_STYLE])
+
+def generateEmptyReport(data):
+	template = env.get_template(SALES_REPORT_TEMPLATE)
+	template_vars = data
+	html_output = template.render(template_vars)
+	HTML(string=html_output).write_pdf(OUTPUT_FOLDER + data['title'] + ".pdf", stylesheets=[DEPLETED_REPORT_STYLE])
