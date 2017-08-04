4249e98638a06f5234169faaabbfe794e45f502d
Eligio Becerra <eligio_bz@mail.com>
2017-08-04 16:44:30 -0500

    Commencing versioning of the database

--- db_versions/versions/001_Add_blah.py
+++ db_versions/versions/001_Add_blah.py
@@ -1,13 +1,21 @@
-from sqlalchemy import *
+from sqlalchemy import Table, Column, Integer, String, MetaData
 from migrate import *
+
+meta = MetaData()
+
+User = Table(
+	'User', meta,
+	Column('id', Integer, primary_key=True),
+	Column('login', String(40)),
+	Column('passwd', String(40)),
+)
 
 
 def upgrade(migrate_engine):
-    # Upgrade operations go here. Don't create your own engine; bind
-    # migrate_engine to your metadata
-    pass
+	meta.bind = migrate_engine
+	User.create()
 
 
 def downgrade(migrate_engine):
-    # Operations to reverse the above upgrade go here.
-    pass
+	meta.bind = migrate_engine
+	User.drop()
