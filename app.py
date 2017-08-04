fafe0c5bb9f848f7ab89d15eb4798d806bed93c6
Eligio Becerra <eligio_bz@mail.com>
2017-08-04 12:47:11 -0500

    testing changes in app to run on Heroku

--- app.py
+++ app.py
@@ -189,4 +189,4 @@ def checkDB():
 if __name__ == '__main__':
     checkDB()
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
