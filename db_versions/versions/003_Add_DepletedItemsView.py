0027df22b3aa9494fe7faee8c49a5fa0c416c35c
Eligio Becerra <eligio_bz@mail.com>
2017-08-04 20:11:45 -0500

    Added script for upgrade to version 3. Need to check how can I enforce a database version

---
+++ db_versions/versions/003_Add_DepletedItemsView.py
@@ -0,0 +1,24 @@
+from sqlalchemy import text, MetaData
+from migrate import *
+
+meta = MetaData()
+
+CreateDepletedItemsView = text("CREATE VIEW DepletedItemsView AS"\
+	" SELECT SaleDetails.idSale, Product.barcode, Product.name, Sale.date"
+	" FROM Product"
+	" JOIN SaleDetails ON SaleDetails.idProduct = Product.barcode"
+	" JOIN Sale ON Sale.id = SaleDetails.idSale"
+	" WHERE Product.units = 0;")
+
+DropDepletedItemsView = text("DROP VIEW DepletedItemsView")
+
+def upgrade(migrate_engine):
+	meta.bind = migrate_engine
+	meta.reflect(views=True)
+	migrate_engine.execute(CreateDepletedItemsView)
+
+
+def downgrade(migrate_engine):
+	meta.bind = migrate_engine
+	meta.reflect(views=True)
+	migrate_engine.execute(DropDepletedItemsView)
