cf6c520d19a380a26788e1f9f5fc30dbedad020a
Eligio Becerra <eligio_bz@mail.com>
2017-12-06 01:35:48 -0600

    Updated manage.py

--- manage.py
+++ manage.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 from migrate.versioning.shell import main
 
+import os
+
 if __name__ == '__main__':
-    main(repository='db_versions', url='sqlite:///mobilerp.db', debug='False')
+    main(repository='db_versions', url=os.environ['DATABASE_URL'], debug='False')
