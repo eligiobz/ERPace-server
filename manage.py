41927ae62932a36c2c11931a5ce7a15ea2d53630
Eligio Becerra <eligio_bz@mail.com>
2017-12-21 17:25:16 -0600

    updated manage.py

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from migrate.versioning.shell import main
+import os
 
 if __name__ == '__main__':
-    main(repository='psql_repo', url='postgresql://mloki:?!L0k13t0@localhost/mobilerp', debug='False')
+    main(repository='psql_repo', url=os.environ['DATABASE_URL'], debug='False')
