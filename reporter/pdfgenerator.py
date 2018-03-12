2aa665029383cdb573dc0863529371808e010f47
Eligio Becerra <eligio_bz@mail.com>
2018-03-12 14:14:02 -0600

    Report generation enabled

--- reporter/pdfgenerator.py
+++ reporter/pdfgenerator.py
@@ -29,23 +29,13 @@ env = Environment(loader=FileSystemLoader('.'),
 env.trim_blocks = True
 env.lstrip_blocks = True
 
-pdf_or_latex = ''
-
-if pypandoc.get_pandoc_version().startswith('2'):
-  pdf_or_latex = '--pdf-engine=xelatex'
-else:
-  pdf_or_latex = '--latex-engine=xelatex'
-
-
 def generateSalesPdf(data):
   template = env.get_template(SALES_REPORT_TEMPLATE)
   template_vars = data
   html_output = template.render(template_vars)
   output = pypandoc.convert_text(html_output, format='html', to='pdf',
-    extra_args=[pdf_or_latex, '-V mainfont="DejaVu Serif"',
-    '-V sansfont=Arial'], outputfile="static/pdf/salesreport.pdf")
+      outputfile="static/pdf/salesreport.pdf")
   
-
 def generateDepletedReport(data):
     template = env.get_template(DEPLETED_REPORT_TEMPLATE)
     template_vars = data
