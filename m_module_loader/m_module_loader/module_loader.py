337f47ad84712cdd47bcbb1dd703cd0621892817
Eligio Becerra <eligio_bz@mail.com>
2018-08-08 21:13:48 -0500

    Started working on a plugin system for the ERPace

---
+++ m_module_loader/m_module_loader/module_loader.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8 -*-
+
+##############################################################################
+# MobilEPR - A small self-hosted ERP that works with your smartphone.
+# Copyright (C) 2018  Eligio Becerra
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##############################################################################
+
+import os
+
+class ModuleLoader():
+
+    __found_dirs__ = []
+
+    def __init__(self):
+        for dirs in os.listdir(os.getcwd()):
+            if (dirs.startswith("m_")):
+                self.__found_dirs__.append(dirs)
+        print (self.__found_dirs__)
